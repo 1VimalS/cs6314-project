0fcc8ff0a5dc9a1ac818ea848ba9441da40ffa9e
1VimalS <vimaljsebastian@gmail.com>
2025-11-17 23:27:52 -0600

    test dependencies and error handling in apis

--- api.js
+++ api.js
@@ -6,32 +6,62 @@ const api = axios.create({
 
 // Users
 export const fetchUsers = async () => {
-  const res = await api.get('/user/list');
-  return res.data;
+  try {
+    const res = await api.get('/user/list');
+    return res.data;
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    throw error;
+  }
 };
 
 export const fetchUser = async (userId) => {
-  const res = await api.get(`/user/${userId}`);
-  return res.data;
+  try {
+    const res = await api.get(`/user/${userId}`);
+    return res.data;
+  } catch (error) {
+    console.error('Error fetching user:', error);
+    throw error;
+  }
 };
 
 export const fetchUserCounts = async (userId) => {
-  const res = await api.get(`/user/${userId}/counts`);
-  return res.data;
+  try {
+    const res = await api.get(`/user/${userId}/counts`);
+    return res.data;
+  } catch (error) {
+    console.error('Error fetching user counts:', error);
+    throw error;
+  }
 };
 
 export const fetchUserComments = async (userId) => {
-  const res = await api.get(`/user/${userId}/comments`);
-  return res.data;
+  try {
+    const res = await api.get(`/user/${userId}/comments`);
+    return res.data;
+  } catch (error) {
+    console.error('Error fetching user comments:', error);
+    throw error;
+  }
 };
 
 // Photos
 export const fetchPhotosOfUser = async (userId) => {
-  const res = await api.get(`/photosOfUser/${userId}`);
-  return res.data;
+  try {
+    const res = await api.get(`/photosOfUser/${userId}`);
+    return res.data;
+  } catch (error) {
+    console.error('Error fetching photos of user:', error);
+    throw error;
+  }
 };
-
+  
 export const fetchPhotoOfUserByIndex = async (userId, index) => {
-  const res = await api.get(`/photosOfUser/${userId}/${index}`);
-  return res.data;
+  try {
+    const res = await api.get(`/photosOfUser/${userId}/${index}`);
+    return res.data;
+  } catch (error) {
+    console.error('Error fetching photo of user by index:', error);
+    throw error;
+  }
 };
